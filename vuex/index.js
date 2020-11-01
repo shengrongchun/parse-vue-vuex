beb2a3f32c5a9459c4ce7da1cb4f526bf0af8d04
shengrongchun325 <shengrongchun325@zto.com>
2020-11-01 11:09:27 +0800

    state-4: 新增辅助函数

--- vuex/index.js
+++ vuex/index.js
@@ -1,8 +1,24 @@
 import { Store, install } from './store'
+import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
 
 // 这种模式 vuex.Store
 export default {
   Store,
   install,
   version: '__VERSION__',
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+}
+// 这种模式 import {Store} from vuex
+export {
+  Store,
+  install,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
 }
